ede6ded0f41a936cd152a85719eb6f002c880597
fatihergl <muratergul2005@gmail.com>
2026-01-28 13:45:26 +0000

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Fatih Murat Ergul")
+class Student:
+    def __init__(self, name ,id):
+        self.name = name
+        self.id = id
+
+s1 = Student("Fatih" , "35239")
+
+print(s1.name)
+print(s1.id)
+
+
+class Car:
+    def __init__(car, brand ,model):
+        car.brand = brand
+        car.model = model
+
+c1 = Car("mercedes" , "c250")
+
+print(c1.model)
+print(c1.brand)
+        
